c48507c79b936486f2f55866c700caeff14cf2fa
jayanta <jayanta.garu@arithaconsulting.com>
2022-11-08 18:35:20 +0530

    code update

--- src/Prasentation/Component/ReactOtpInput/ReactOtpInput.js
+++ src/Prasentation/Component/ReactOtpInput/ReactOtpInput.js
@@ -1,51 +1,88 @@
-import {View, Text, TextInput, ProgressViewIOSComponent} from 'react-native';
-import React from 'react';
+import {
+  View,
+  Text,
+  TextInput,
+  ProgressViewIOSComponent,
+  Platform,
+  TouchableOpacity,
+} from 'react-native';
+import React, {useRef, useState} from 'react';
 import styles from './styles';
+import {
+  guidelineBaseWidth,
+  scale,
+} from '../../../Infrastructure/utils/screenUtility';
 
-const ReactOtpInput = props => {
-  return (
-    <>
-      <View style={styles.container}>
+const OtpInput = props => {
+  const length = 4;
+  const TextInputss = [];
+  let pin = [];
+  console.log('🚀 ~ file: ReactOtpInput.js ~ line 19 ~ OtpInput ~ pin', pin);
+  const mapRef = [];
+  for (let index = 0; index < length; index++) {
+    mapRef.push(useRef());
+  }
+  const TextInputs = () => {
+    for (let i = 0; i < length; i++) {
+      TextInputss.push(
         <View
           style={{
-            flex: 1,
-            flexDirection: 'row',
-            justifyContent: 'center',
-            alignItems: 'center',
-            flexWrap: 'wrap',
+            borderWidth: scale(0.5),
+            borderRadius: scale(2),
+            backgroundColor: '#FFFFFF',
+            marginHorizontal:
+              Platform.isPad || guidelineBaseWidth > 500 ? scale(40) : scale(0),
+            marginTop:
+              Platform.isPad || guidelineBaseWidth > 500 ? scale(20) : scale(0),
           }}>
-          {/* <Text>ReactOtpInput</Text> */}
           <TextInput
-            name="userID"
-            // placeholder="Enter"
-            placeholderTextColor="#4D4F5C"
-            value={props.value}
-            onBlur={props.onBlur ? props.onBlur : null}
-            onChangeText={props.onChangeText}
-            style={props.style ? props.style : styles.TextInput}
+            style={{
+              height: scale(40),
+              width: scale(40),
+              margin: scale(4),
+              textAlign: 'center',
+              fontSize: scale(22),
+              fontWeight: '500',
+              color: '#000000',
+              borderRadius: scale(6),
+              backgroundColor: '#F7FOFF',
+              paddingBottom: 0,
+              paddingTop: 0,
+            }}
+            value={pin[i]}
+            ref={mapRef[1]}
+            kev={1}
+            autoCorrect={false}
+            autoFocus={i === 0}
+            maxLength={1}
+            keyboardType="number-pad"
+            // editable={!isError}
           />
-          <TextInput
-            name="userID"
-            // placeholder="Enter"
-            placeholderTextColor="#4D4F5C"
-            value={props.value}
-            onBlur={props.onBlur ? props.onBlur : null}
-            onChangeText={props.onChangeText}
-            style={props.style ? props.style : styles.TextInput}
-          />
-          <TextInput
-            name="userID"
-            // placeholder="Enter"
-            placeholderTextColor="#4D4F5C"
-            value={props.value}
-            onBlur={props.onBlur ? props.onBlur : null}
-            onChangeText={props.onChangeText}
-            style={props.style ? props.style : styles.TextInput}
-          />
-        </View>
+        </View>,
+      );
+    }
+    return (
+      <View
+        style={{
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+          marginTop: scale(10),
+          marginHorizontal: scale(30),
+        }}>
+        {TextInputss}
+      </View>
+    );
+  };
+  return (
+    <>
+      <View
+        style={{
+          marginVertical: scale(10),
+        }}>
+        <TextInputs />
       </View>
     </>
   );
 };
 
-export default ReactOtpInput;
+export default OtpInput;
